abc5af13f648af0d68f6325d5983f363576003e3
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2013-03-11 21:21:18 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R2pp::Application.config.secret_token = 'd2bf6113e14f59101ec953ea3851f01dc72a8c5e535ac15bdfabc0e5d2fb4e9233472579729a2980429bc216a286dc5ba61a30a8efd7faf002f70ab20495a7f7'
