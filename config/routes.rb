f3fcee03ed0beed15f502f3366366f07a09f8967
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2013-03-15 18:51:55 -0300

    Add Users controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 R2pp::Application.routes.draw do
   
-  match '/home',  to: 'static_pages#home'
-  match '/about', to: 'static_pages#about'
+  match '/signup',  to: 'users#new'
+
+  match '/home',    to: 'static_pages#home'
+  match '/about',   to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
